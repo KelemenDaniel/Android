31190ed39d95191f0a6dba2695c2a36edf7548c3
KelemenDaniel <kelemen.daniel@student.ms.sapientia.ro>
2024-10-09 15:13:17 +0300

    Labor2 3. feladat

--- labor2/main.kt
+++ labor2/main.kt
@@ -1,5 +1,7 @@
 package labor2
 
+import java.util.*
+
 fun main(){
     //1
 //    val dict: IDictionary = DictionaryProvider.createDictionary(DictionaryType.HASH_SET)
@@ -13,18 +15,65 @@ fun main(){
 //        }
 //        println("Result: ${word?.let { dict.find(it) }}")
 //    }
-    //2
-    val name = "John Smith"
-    println(name.nameMonogram())
+//2
+//    val name = "John Smith"
+//    println(name.nameMonogram())
+//
+//    val list = mutableListOf<String>("apple", "pear", "melon")
+//
+//    println(list.joinElements("#"))
+//    println(list.getLongestElement())
+//}
+//
+//fun String.nameMonogram(): String{
+//    return this.split(" ").map { it[0] }.joinToString("")
+    //3
+    val validDates = mutableListOf<Date>()
+    val random = Random()
 
-    val list = mutableListOf<String>("apple", "pear", "melon")
+    while (validDates.size < 10) {
+        val year = random.nextInt(2024 - 1900) + 1900 // Range: 1900 to 2023
+        val month = random.nextInt(12) + 1            // Range: 1 to 12
+        val day = random.nextInt(31) + 1              // Range: 1 to 31 (not all will be valid)
 
-    println(list.joinElements("#"))
-    println(list.getLongestElement())
-}
+        val date = Date(year, month, day)
 
-fun String.nameMonogram(): String{
-    return this.split(" ").map { it[0] }.joinToString("")
+        // Check if the date is valid
+        if (date.isValid()) {
+            validDates.add(date)
+        } else {
+            println("Invalid date: $date")
+        }
+    }
+
+    println("Valid dates:")
+    validDates.forEach { println(it) }
+
+    println("<----------------------->")
+
+    validDates.sort()
+    println("Sorted valid dates:")
+    validDates.forEach { println(it) }
+
+    validDates.reverse()
+
+    println("<----------------------->")
+
+    println("Reverse sorted valid dates:")
+    validDates.forEach { println(it) }
+
+    val customDateComparator = Comparator<Date> { date1, date2 ->
+        when {
+            date1.year != date2.year -> date1.year - date2.year
+            date1.month != date2.month -> date1.month - date2.month
+            else -> date1.day - date2.day
+        }
+    }
+
+    validDates.sortWith(customDateComparator)
+    println("<----------------------->")
+    println("Custom sorted valid dates:")
+    validDates.forEach { println(it) }
 }
 
 fun List<String>.joinElements(separator: String): String = this.joinToString(separator)
