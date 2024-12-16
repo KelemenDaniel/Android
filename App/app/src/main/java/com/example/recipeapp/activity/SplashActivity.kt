de6333223dd50c5c12d6bd3e66fd6da0cca83d59
KelemenDaniel <kelemen.daniel@student.ms.sapientia.ro>
2024-12-16 15:31:45 +0200

    Logo animation, navigation bar background

--- App/app/src/main/java/com/example/recipeapp/activity/SplashActivity.kt
+++ App/app/src/main/java/com/example/recipeapp/activity/SplashActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
 import android.util.Log
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,21 +18,37 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         Log.d("Splash", "onCreate() called!")
         val handlerThread = HandlerThread("SplashHandlerThread", -10)
         handlerThread.start() // Create a Handler on the new HandlerThread
         val handler = Handler(handlerThread.looper)
         val SPLASH_TIME_OUT = 2000L // Delay in milliseconds
+
         handler.postDelayed({
-        // Navigate to MainActivity after the delay
-            val intent = Intent(this@SplashActivity, MainActivity::class.java)
-            startActivity(intent)
-            finish() },
-            SPLASH_TIME_OUT)
+            // Start move-up and spin animation for the logo
+            val logo = findViewById<ImageView>(R.id.imageView)
+            val moveUpSpin = AnimationUtils.loadAnimation(this, R.anim.logo_anim)
+            logo.startAnimation(moveUpSpin)
+
+            // Navigate to MainActivity after the animation
+            moveUpSpin.setAnimationListener(object : android.view.animation.Animation.AnimationListener {
+                override fun onAnimationStart(animation: android.view.animation.Animation?) {}
+
+                override fun onAnimationEnd(animation: android.view.animation.Animation?) {
+                    val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+
+                override fun onAnimationRepeat(animation: android.view.animation.Animation?) {}
+            })
+        }, SPLASH_TIME_OUT)
     }
 }
